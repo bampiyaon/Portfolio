85a3c8eb5cf600d62b5489205e447165695eef95
bampiyaon <piyaon-p@hotmail.com>
2021-05-15 23:07:49 +0700

    add owl carousel theme

--- resources/js/script.js
+++ resources/js/script.js
@@ -12,4 +12,32 @@ $(document).ready(function() {
         $('.navbar .menu').toggleClass("active"); //to make hamburger menu click function
         $('.menu-btn i').toggleClass("active");
     });
+
+    // owl carousel script
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+	    autoplay: true,
+	    margin: 20,
+	    animateOut: 'fadeOut',
+	    animateIn: 'fadeIn',
+	    nav: true,
+	    dots: true,
+        responsiveClass:true,
+	    items: 1,
+        autoplayHoverPause:true,
+	    navText: ["<i class='fa fa-chevron-left'></i>","<i class='fa fa-chevron-right'></i>"],
+        
+        responsive:{
+            0:{
+                items:1,
+            },
+            600:{
+                items:2,
+            },
+            1000:{
+                items:3,
+                loop:true
+            }
+        }
+    });
 });
